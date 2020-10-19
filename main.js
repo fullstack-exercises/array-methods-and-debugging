fa538865dc79a612cb5aeeda97e11619d3a3f9fa
Jessica Michel <jessicamichel93@gmail.com>
2020-10-19 16:03:55 +0200

    transformed normal functions to arrow functions

---
+++ main.js
@@ -0,0 +1,23 @@
+// Part A
+const ikRockArrowFunctions = () => {
+    console.log("Joe, ik rock de arrow functions!");
+};
+
+ikRockArrowFunctions();
+
+// Part B
+// Opdracht: schrijf de onderstaande functie om naar een arrow functie op 1 regel.
+const fivePlusSeven = () => console.log(5 + 7);
+fivePlusSeven();
+
+// Part C
+const myFunction = (a, b) => a + b;
+myFunction();
+
+// Part D
+const myFunction2 = a => a;
+myFunction2();
+
+// Part E
+const createObject = () => ({ greeting: "hoi" });
+console.log(createObject());
