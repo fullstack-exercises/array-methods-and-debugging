c188c527be5b469c5339d3be753a8f5f36cd4e7d
Jessica Michel <jessicamichel93@gmail.com>
2020-10-19 16:48:06 +0200

    debugging, added console-log

--- main.js
+++ main.js
@@ -1,23 +1,19 @@
-// Part A
-const ikRockArrowFunctions = () => {
-    console.log("Joe, ik rock de arrow functions!");
-};
+const array = [
+    { name: "N. Armstrong", profession: "spacecowboy", age: 89 },
+    { name: "H. de Haan", profession: "kippen hypnotiseur", age: 59 },
+    { name: "A. Curry", profession: "kikvorsman", age: 32 },
+    { name: "F. Vonk", profession: "slangenmelker", age: 36 },
+    { name: "B. Bunny", profession: "konijnen uitlaatservice", age: 27 },
+    { name: "Dr.Evil", profession: "digital overlord", age: 56 }
+];
 
-ikRockArrowFunctions();
+let currentYear = new Date().getFullYear();
 
-// Part B
-// Opdracht: schrijf de onderstaande functie om naar een arrow functie op 1 regel.
-const fivePlusSeven = () => console.log(5 + 7);
-fivePlusSeven();
-
-// Part C
-const myFunction = (a, b) => a + b;
-myFunction();
-
-// Part D
-const myFunction2 = a => a;
-myFunction2();
-
-// Part E
-const createObject = () => ({ greeting: "hoi" });
-console.log(createObject());
+for (let person of array) {
+    if (person.age >= 50) {
+        // console.log("Dit is de gehele persoon:", person);
+        // console.log("Dit is", person.name);
+        // console.log("Geboren in", currentYear - person.age);
+        console.log(person.name + " is een " + person.profession);
+    }
+}
