70f7584a4887be212974fe77f74ab96f013334a5
Jessica Michel <jessicamichel93@gmail.com>
2020-10-20 17:19:35 +0200

    Array Methods exercises part 2

--- main.js
+++ main.js
@@ -1,51 +1,55 @@
 // Part A
-const addTheWordCool = () => {
-    let words = ["nice", "awesome", "tof"];
-    words.push('cool');
-    return words;
-}
-console.log(addTheWordCool()); // result: ["nice", "awesome", "tof", "cool"]
+const superheroes = [
+    { name: "Batman", alter_ego: "Bruce Wayne" },
+    { name: "Superman", alter_ego: "Clark Kent" },
+    { name: "Spiderman", alter_ego: "Peter Parker" }
+]
+
+const findSpiderMan = superheroes.find((item) => {
+    return item.name === 'Spiderman';
+});
+
+console.log(findSpiderMan); // result: {name: "Spiderman", alter_ego: "Peter Parker"}
 
 // Part B
-const returnItems = () => {
-    let items = ['appels', 'peren', 'citroenen'];
-    return items.length;
-}
-console.log(returnItems()); // 3
+const numbers = [1, 2, 3];
+numbers.forEach((item) => {
+    console.log(item * 2); // result: [2, 4, 6]
+});
 
 // Part C
-const returnFirstItem = () => {
-    let items = ['Belgie', 'Nederland', 'Luxenburg'];
-    return items[0];
-}
-console.log(returnFirstItem()); // result: Belgie
+containsNumberBiggerThan10 = [1, 4, 3, 6, 9, 7, 11];
+const checkNumber = containsNumberBiggerThan10.some((number) => {
+    return number < 10;
+});
+console.log(checkNumber); // result: true
 
 // Part D
-const returnLastItem = () => {
-    let items = ['Haas', 'Cavia', 'Kip', 'Schildpad'];
-    return items[items.length - 1];
+const isItalyInTheGreat7 = ['Canada', 'France', 'Germany', 'Italy', 'Japan', 'United Kingdom', 'United States'];
+const checkCountry = () => {
+    console.log(includes('Italy'));
 }
-console.log(returnLastItem()); // result: Schildpad
+checkCountry; // result: true
 
 // Part E
-const presidents = ["Trump", "Obama", "Bush", "Clinton"]
-const dissapearFirstIteminArray = () => {
-    return presidents.slice(2)
-}
-console.log(dissapearFirstIteminArray()); // result: [ 'Obama', 'Bush', 'Clinton' ]
+const numbersExerciseE = [1, 4, 3, 6, 9, 7, 11];
+let multiplyBy10 = [];
+
+numbersExerciseE.forEach((number) => {
+    multiplyBy10.push(number * 10);
+});
+console.log(multiplyBy10); // Result: [10, 40, 30, 60, 90, 70, 110]
 
 // Part F
-const stringsTogether = () => {
-    let words = ['Winc', 'Academy', 'is', 'leuk', ';-}'];
-    return words.join();
-}
-console.log(stringsTogether()); // result: Winc,Academy,is,leuk,;-}
+const numbersBelow100 = [1, 81, 4, 53, 3, 6, 79, 2, 43, 7, 28, 101, 11, 77, 84, 98];
+const isBelow100 = numbersBelow100.includes((number) => {
+    return number <= 100;
+});
+console.log(isBelow100); // return: false
 
-// Part G
-const combineArrays = () => {
-    array1 = [1, 2, 3];
-    array2 = [4, 5, 6];
-    combine = [...array1, ...array2];
-    return combine;
-}
-console.log(combineArrays()); // result: [ 1, 2, 3, 4, 5, 6 ]
+// Part G BONUS
+const bigSum = [1, 81, 4, 53, 3, 6, 79, 2, 43, 7, 28, 11, 77, 84, 98, 101, 206, 234];
+const totalOfBigSum = bigSum.reduce((number, currentValue) => {
+    return number + currentValue;
+});
+console.log(totalOfBigSum); // return: 1118
